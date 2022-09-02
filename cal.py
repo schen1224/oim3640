45e2fd1e655ccae127684ff0e7d3d430edf34348
schen1224 <schen9@babson.edu>
2022-09-02 15:32:55 -0400

    Excercise02

---
+++ cal.py
@@ -0,0 +1,14 @@
+# How many seconds are there in 42 minutes 42 seconds?
+print("Question 1 answer:")
+print(42 + 42 * 60)
+# How many miles are there in 10 kilometers? Hint: there are 1.61 kilometers in a mile
+print("Question 2 answer:")
+print(10 * 1.61)
+# If you run a 10 kilometer race in 42 minutes 42 seconds, what is you average pace? What is youraverage speed in miles per hour?
+print("Question 3 answer:")
+print("time per mile in minutes:")
+print(2562 / 16.1 / 60)
+print("time per mile in seconds:")
+print(2562 / 16.1)
+print("speed in miles per hour:")
+print(16.1 / 2562 * 60 * 60)
---
+++ hello.py
@@ -0,0 +1,4 @@
+print("hello,world!")
+print(2*3)
+
+print(2+2)
